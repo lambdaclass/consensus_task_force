941b0e2de0c404a5a598b399f1cd1e0dbac3f490
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-01-05 17:04:35 -0300

    Add ping function

--- lib/basic_node.ex
+++ lib/basic_node.ex
@@ -1,10 +1,23 @@
 defmodule BasicNode do
   use GenServer
 
+  ##########################
+  ### Public API
+  ##########################
+
   @spec start_link(any()) :: GenServer.on_start()
   def start_link(init_args) do
     GenServer.start_link(__MODULE__, init_args)
   end
+
+  @spec ping(pid(), pid()) :: :ok
+  def ping(pid, from \\ self()) do
+    GenServer.cast(pid, {:ping, from})
+  end
+
+  ##########################
+  ### GenServer Callbacks
+  ##########################
 
   @impl GenServer
   def init(_init_args) do
